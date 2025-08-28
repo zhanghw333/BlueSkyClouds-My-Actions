a758792ecbe39fd51379c235d2964e2886e05ec8
zhanghw333 <891073572@qq.com>
2025-08-28 14:49:35 +0800

    Update bilibili.py

--- function/bilibili/bilibili.py
+++ function/bilibili/bilibili.py
@@ -20,55 +20,6 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import make_column_transformer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-df=pd.read_csv('/Users/zhanghanwen/Desktop/ABtest.csv')
-
-group_mapping = {
-    '对照组': 'A',
-    '实验组1': 'B',
-    '实验组2': 'C', 
-    '实验组3': 'D'
-}
-
-df['new_group'] = df['group'].map(group_mapping)
-
-from sklearn.preprocessing import LabelEncoder
-
-# 创建LabelEncoder对象
-le = LabelEncoder()
-
-# 拟合并转换
-df['user_type_labelencoded'] = le.fit_transform(df['User_os'])
-
-df['Favorited_songs_typelabelencoded'] = le.fit_transform(df['Favorited_songs_type'])
-
-
-X = df.drop(['User_id','Is_purchased_member','new_group','group','User_os',
-             'Favorited_songs_type','Is_download','Is_confirm','Is_exclusive_music'
-             ,'Is_vip_music_quality'
-             ], axis=1)
-y = df['Is_purchased_member']
-variant = df['new_group'].astype('category').cat.codes 
-
-variant_multi = np.column_stack([(variant == i).astype(float) for i in [1, 2, 3]])
-
-
-discrete_features = ['user_type_labelencoded', 'Favorited_songs_typelabelencoded', 'Is_mobile_data',
-  'Is_buy_album']
-continuous_features = ['Register_days', 'Listening_days', 'Listening_duration'
-    ,'Songs_count','Favorited_songs_count','Mobile_data_listening_duration',
-    'Search_count']
-
-
-preprocessor = make_column_transformer(
-    (StandardScaler(), continuous_features),
-    (OneHotEncoder(handle_unknown='ignore'), discrete_features)
-)
-
-# 应用预处理
-X_processed = preprocessor.fit_transform(X)
-
-X_train, X_test, y_train, y_test, variant_train, variant_test = train_test_split(
-    X_processed, y, variant_multi, test_size=0.2, random_state=42)
 
 
 from econml.dml import CausalForestDML
@@ -86,23 +37,8 @@ cf = CausalForestDML(
     min_samples_split=20
 )
 
-cf.fit(X=X_train, T=variant_train, Y=y_train)
+
 
 
 #预测处理效应：
-
-to_pred=cf.effect(X_processed)
-
-lift_values = to_pred[:, :-1] - to_pred[:, 0:]  # D/B/C vs A的提升值
-#计算ATE
-#？算每列的平均值？
-ate=to_pred.mean()
-#为每个用户推荐最佳弹窗
-best_strategy = np.argmax(lift_values, axis=1)
-strategy_names = ['A', 'B', 'C', 'D']
-df['recommended_popup'] = strategy_names[best_strategy]
-
-
-
-
 
