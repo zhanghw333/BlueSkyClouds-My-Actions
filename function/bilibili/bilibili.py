b645d5aa12d10c680c77e01538785166eb68990a
zhanghw333 <891073572@qq.com>
2025-08-28 22:45:38 +0800

    Update bilibili.py

--- function/bilibili/bilibili.py
+++ function/bilibili/bilibili.py
@@ -84,7 +84,7 @@ cf = CausalForestDML(
 
 cf.fit(X=X_train, T=variant_train, Y=y_train)
 
-effect_matrix = cf.effect(X=X_test, T0=0，T1=2)
+effect_matrix = cf.effect(X=X_test,T0=0,T1=2)
 
 print(effect_matrix)
 
