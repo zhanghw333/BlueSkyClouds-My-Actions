7bdcb44f3069711d5dd389fc61f739b6600e4655
zhanghw333 <891073572@qq.com>
2025-08-30 19:30:58 +0800

    Update bilibili.py

--- function/bilibili/bilibili.py
+++ function/bilibili/bilibili.py
@@ -131,3 +131,13 @@ dt = DecisionTreeClassifier(
     random_state=42
 )
 dt.fit(X, y)
+plot_tree(
+    dt,
+    feature_names=all_columns.tolist(),  # 替换为你的特征名
+    class_names=[1, 2, 3],           # 处理编号
+    filled=True,                    # 填充颜色表示纯度
+    rounded=True,                   # 圆角矩形
+    fontsize=12                    # 字体大小
+)
+plt.savefig("icon/decision_tree_rules.png")  # 保存图片
+plt.show()
